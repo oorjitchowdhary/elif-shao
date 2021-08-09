6c497d99935d817c298ecef5666f34da8c80d6f1
ahmedmeshref <a.meshref@alustudent.com>
2021-08-09 02:54:13 +0200

    Implement utils functions

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,12 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { DateUtils } from './utils/utils';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
+
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "elif" is now active!');
@@ -24,10 +25,13 @@ export function activate(context: vscode.ExtensionContext) {
 	// change between dark/light theme based on time  
 	let changeTheme = vscode.commands.registerCommand('elif.changeTheme', () => {
 		vscode.window.showInformationMessage('I will change your theme now!');
+		vscode.commands.executeCommand('vscode.openFolder');
 	});
 
 	context.subscriptions.push(changeTheme);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
+
+
